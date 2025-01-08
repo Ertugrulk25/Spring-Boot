b20906805ee6be0f058b4c1b3b5b1562d583be8c
nesibe <tnesibe.techproed@gmail.com>
2025-01-08 14:10:14 +0300

    save_update

--- DT/07- SpringMVC/SpringMvc/src/main/java/com/tpe/controller/StudentController.java
+++ DT/07- SpringMVC/SpringMvc/src/main/java/com/tpe/controller/StudentController.java
@@ -1,9 +1,15 @@
 package com.tpe.controller;
 
+import com.tpe.domain.Student;
+import com.tpe.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller//requestler bu classta karşılanacak ve ilgili metodlarla maplenecek
 @RequestMapping("/students")//http:localhost:8080/SpringMvc/students/....
@@ -11,10 +17,10 @@ import org.springframework.web.servlet.ModelAndView;
 //method:sadece ilgili metodu requestle mapler
 public class StudentController {
 
+    @Autowired
+    private IStudentService service;
 
     //NOT:controllerda metodlar geriye mav veya String data tipi döndürebilir.
-
-
 
     //http:localhost:8080/SpringMvc/students/hi + GET--okuma
     //http:localhost:8080/SpringMvc/students/hi + POST--kayıt
@@ -36,11 +42,58 @@ public class StudentController {
     //http://localhost:8080/SpringMvc/students + GET
     @GetMapping
     public ModelAndView getStudents(){
+        List<Student> allStudent=service.listAllStudents();
         ModelAndView mav=new ModelAndView();
-        //todo:dbden liste gelecek...
+        mav.addObject("studentList",allStudent);
         mav.setViewName("students");
         return mav;
     }
+
+    //2-öğrenciyi kaydetmek için form gösterme
+    //request: http://localhost:8080/SpringMvc/students/new + GET
+    //response:form göstermek
+    @GetMapping("/new")
+    public String sendForm(@ModelAttribute("student") Student student){
+        return "studentForm";
+    }
+    //ModelAttribute anotasyonu view katmanı ile controller arasında
+    //modelın transferini sağlar.
+
+    //işlem sonunda: studentın firstname,lastname ve grade değerleri set edilmiş halde
+    //controller classında yer alır
+
+    //2-b:formun içindeki öğrenciyi kaydetme
+    //request:http://localhost:8080/SpringMvc/students/saveStudent + POST
+    //response:öğrenciyi tabloya ekleyeceğiz ve liste göndereceğiz
+    @PostMapping("/saveStudent")
+    public String addStudent(@Valid @ModelAttribute("student")Student student, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            return "studentForm";
+        }
+
+        service.addOrUpdateStudent(student);
+
+        return "redirect:/students";//http://localhost:8080/SpringMvc/students + GET
+
+    }
+
+    //3-öğrenciyi güncelleme
+    //request:http://localhost:8080/SpringMvc/students/update?id=3 + GET
+    //response:update için id si verilen öğrencinin bilgileri ile formu gösterme
+    //idsi verilen öğrenciyi bulmamız gerekir...
+    @GetMapping("/update")
+    public ModelAndView sendFormUpdate(@RequestParam("id") Long identity){
+
+        Student foundStudent= service.findStudentById(identity);
+
+        ModelAndView mav=new ModelAndView();
+        mav.addObject("student",foundStudent);
+        mav.setViewName("studentForm");
+        return mav;
+    }
+
+
 
 
 
